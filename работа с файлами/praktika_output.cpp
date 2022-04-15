9d57d8447ecc489ebe09e722254780303b6a0693
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2022-04-15 19:21:21 +0300

    Add files via upload

---
+++ работа с файлами/praktika_output.cpp
@@ -0,0 +1,36 @@
+﻿// praktika.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+
+#include <fstream>/*ifstream ofstream*/
+#include <string.h>
+#include <sstream>
+using namespace std;
+int main()
+{ 
+   /*чтение из файла*/
+    fstream f;
+    string path="myfile.txt";
+    f.open(path);
+    if (!f.is_open())
+    {
+        cout << "oops!";
+    }
+    else
+    {
+        cout << "yhu!";
+     
+        string ch;
+         
+     
+        while (!f.eof())
+        {
+            ch = "";
+            getline(f, ch);
+            cout << ch<<endl;
+        }
+
+    }
+    f.close();
+}
+
