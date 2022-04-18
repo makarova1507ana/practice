2a179974c8f894221576a3878fe7ecfd4e624a0d
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2022-04-18 17:29:02 +0300

    добавлен пример по работе с символами

---
+++ работа с файлами/praktika_symbol.cpp
@@ -0,0 +1,34 @@
+﻿// praktika.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+
+#include <fstream>/*ifstream ofstream*/
+#include <string.h>
+#include <sstream>
+using namespace std;
+int main()
+{ 
+   /*чтение из файла*/
+    fstream f;
+    string path="myfile.txt";
+    f.open(path);
+    
+    if (!f.is_open())
+    {
+        cout << "oops!";
+    }
+    else
+    {
+        cout << "yhu!";
+     
+        char ch;
+        while (f.get(ch))/* пока считывается символ */
+        {
+            f >> ch;/*считываем символ  */
+            cout << ch<<endl;
+        }
+
+    }
+    f.close();
+}
+
